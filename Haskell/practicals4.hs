08f6f7dacba2487fb2db4992cdc1849253f83b1b
Yung-Luen Lan <yllan@me.com>
2014-07-01 09:05:51 +0800

    Change Int to N in practicals 4

--- Haskell/practicals4.hs
+++ Haskell/practicals4.hs
@@ -1,20 +1,26 @@
+data N = Zero | Succ N deriving Show
+
+add :: N -> N -> N
+add Zero n = n
+add (Succ m) n = Succ (add m n)
+
 -- 4.1
-mul :: Int -> Int -> Int
-mul 0 n = 0
-mul m n = n + mul (m - 1) n
+mul :: N -> N -> N
+mul Zero n = Zero
+mul (Succ m) n = add n (mul m n)
 
 -- 4.2
-myMin :: Int -> Int -> Int
-myMin 0 n = n
-myMin m 0 = m
-myMin m n = 1 + myMin (m - 1) (n - 1)
+myMin :: N -> N -> N
+myMin Zero n = Zero
+myMin m Zero = Zero
+myMin (Succ m) (Succ n) = Succ (myMin m n)
 
 -- 4.3
-elemAt :: Int -> [a] -> a
-elemAt 0 (x:xs) = x
-elemAt n (x:xs) = elemAt (n - 1) xs
+elemAt :: N -> [a] -> a
+elemAt Zero (x:xs) = x
+elemAt (Succ n) (x:xs) = elemAt n xs
 
 -- 4.4
-insertAt :: Int -> a -> [a] -> [a]
-insertAt 0 e xs = e : xs
-insertAt n e (x:xs) = x : insertAt (n - 1) e xs
+insertAt :: N -> a -> [a] -> [a]
+insertAt Zero e xs = e : xs
+insertAt (Succ n) e (x:xs) = x : insertAt n e xs
